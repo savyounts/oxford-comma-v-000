6ed7e7465914cda93c29d173c85facc5c891fa9e
Sav Younts <savyounts@gmail.com>
2018-08-03 18:36:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,11 @@
 def oxford_comma(array)
  
+ if array.size > 2 
   array[-1] = "and " + array.last
-  
   array.join(", ")
+ elsif array.size == 2 
+  array.join(" and ")
+else
+  array.to_s 
+end
 end
